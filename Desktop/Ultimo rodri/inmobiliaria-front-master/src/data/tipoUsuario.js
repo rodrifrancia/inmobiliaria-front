75cdc69fd02494d96223f13f7c4f2e3ea0907c94
rodrifrancia <67762151+rodrifrancia@users.noreply.github.com>
2023-05-22 09:37:38 -0300

    Revert "Jesus (#8)"
    This reverts commit f9de3facf7592c37cdb8394302a6f8581085c284.

--- Desktop/Ultimo rodri/inmobiliaria-front-master/src/data/tipoUsuario.js
+++
@@ -1,7 +0,0 @@
-export async function fetchObtenerTipoUsuario(){
-
-    const respuesta = await fetch(`${import.meta.env.VITE_API_URL}/fetchTipoUsuario`)
-    const resultado = await respuesta.json()
-    return resultado
-}
-
