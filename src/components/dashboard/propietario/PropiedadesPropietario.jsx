42378f1c8f48801fa9a28be653b405252ab33f26
edificor <desarrollosedificor@gmail.com>
2023-05-22 09:48:34 -0300

    lunes aamañana

---
+++ src/components/dashboard/propietario/PropiedadesPropietario.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from '../../../../Desktop/Ultimo rodri/inmobiliaria-front-master/src/components/reutilizables/Footer';
+import Header from '../../../../Desktop/Ultimo rodri/inmobiliaria-front-master/src/components/reutilizables/Header';
+
+function PropiedadesPropietario() {
+  return (
+    <div>
+      <Header />
+      {/* Resto del contenido */}
+      <Footer />
+    </div>
+  );
+}
+
+export default PropiedadesPropietario;
